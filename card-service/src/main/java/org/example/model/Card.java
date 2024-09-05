d33eb2af12d4d13bfbb1a7eb2a0cdd74f5eb5f30
sokhim <retsokhim21@gmail.com>
2024-09-05 18:41:59 +0700

    done

--- card-service/src/main/java/org/example/model/Card.java
+++ card-service/src/main/java/org/example/model/Card.java
@@ -4,16 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Setter
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity(name = "cards")
+@Builder
 public class Card {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
